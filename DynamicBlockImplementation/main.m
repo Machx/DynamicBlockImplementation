01d761aead5c73482fb27c57104dee0fd29d2010
Colin Wheeler <cocoasamurai@gmail.com>
2011-09-16 16:27:16 -0500

    Initial Commit

---
+++ DynamicBlockImplementation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DynamicBlockImplementation
+//
+//  Created by Colin Wheeler on 9/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
